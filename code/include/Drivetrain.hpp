3d957c6206cc1d013182fad9612db4773794c427
Jeremy Cope <jcope0787@protonmail.com>
2024-07-25 19:38:01 -0700

    added basic setup

---
+++ code/include/Drivetrain.hpp
@@ -0,0 +1,19 @@
+#ifndef DRIVETRAIN_HEADER_Y2425_HPP
+#define DRIVETRAIN_HEADER_Y2425_HPP
+
+#include "api.h"
+using namespace pros;
+using namespace pros::literals;
+
+namespace Y2425
+{
+class Drivetrain
+{
+        private:
+                int *mtrports;
+                Motor *mtrs;
+                
+};
+}
+
+#endif //DRIVETRAIN_HEADER_Y2425_HPP
---
+++ code/include/Robot.hpp
@@ -0,0 +1,10 @@
+#ifndef ROBOT_HEADER_Y2425_HPP
+#define ROBOT_HEADER_Y2425_HPP
+
+namespace Y2425
+{
+class Robot {
+        public:
+};
+}
+#endif // ROBOT_HEADER_Y2425_HPP
